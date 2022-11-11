554476627f900f81fea9a50904454a9bb9242ad8
wewechoo <46195268+wewechoo@users.noreply.github.com>
2022-11-11 22:04:07 +0800

    Add files via upload

---
+++ src/Controller/LoginManager.java
@@ -0,0 +1,43 @@
+package Controller;
+
+import java.io.*;
+
+import Model.Admin;
+
+import java.util.ArrayList;
+
+public class LoginManager {
+        public final static String filename = "database/login.txt";
+
+        public void createAccount(String username, String password) {
+            try {
+                Admin admin = new Admin(username, password);
+                ArrayList<Admin> adminList = new ArrayList<Admin>();
+                File loginFile = new File(filename);
+                if (loginFile.exists())
+                    adminList = read();
+                ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename));
+                adminList.add(admin);
+                out.writeObject(adminList);
+                out.flush();
+                out.close();
+                } catch (IOException e) {
+                    //
+                }
+        }
+        public ArrayList<Admin> read() {
+            try {
+                ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+                ArrayList<Admin> adminList = (ArrayList<Admin>) in.readObject();
+                in.close();
+                return adminList;
+            } catch (ClassNotFoundException | IOException e) {
+                // ignore error
+            }
+            return new ArrayList<Admin>();
+        }
+
+
+
+}
+
